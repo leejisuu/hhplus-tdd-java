7cfb6f70cbd84efc0ab0d21566bf7c77bc023d85
leejisuu <dlwltn966@gmail.com>
2024-12-20 01:04:05 +0900

    Refactor: 포인트 충전, 사용 로직에 동시성 제어 로직 추가
    동시성 제어 요구사항
    * 서로 다른 유저에 대한 요청은 각각 동시에 실행된다.
    * 같은 유저에 대한 요청은 한번에 하나만 실행된다.

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 @RequiredArgsConstructor
 @Service
@@ -14,56 +16,70 @@ public class PointService {
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
 
-    // [정책] 유저가 보유 할 수 있는 최대 포인트는 100만 포인트이다.
+    private final ConcurrentHashMap<Long, ReentrantLock> userLocks = new ConcurrentHashMap<>();
+
     private final long MAX_AVAILABLE_POINT = 1_000_000L;
 
     public UserPoint selectUserPointById(long id) {
-
         return userPointTable.selectById(id);
     }
 
-    public List<PointHistory> selectPointHistoryListByUserId(long id) {
+    public List<PointHistory> selectPointHistoriesByUserId(long id) {
 
         return pointHistoryTable.selectAllByUserId(id);
     }
 
-    public UserPoint chargeUserPoint(long id, long amount, long updateMillis) {
-        if(amount <= 0) {
-            throw new RuntimeException("충전 요청 금액은 0원보다 커야 합니다.");
+    public UserPoint chargeUserPoint(long id, long amount) {
+        ReentrantLock lock = userLocks.computeIfAbsent(id, userId -> new ReentrantLock(true));
+
+        lock.lock();
+        try {
+            if(amount <= 0) {
+                throw new RuntimeException("충전 요청 금액은 0원보다 커야 합니다.");
+            }
+            if(amount > MAX_AVAILABLE_POINT) {
+                throw new RuntimeException("충전 요청 금액은 최대 100만원을 초과할 수 없습니다.");
+            }
+
+            UserPoint originUserPoint = userPointTable.selectById(id);
+            if(originUserPoint.point() + amount > MAX_AVAILABLE_POINT) {
+                throw new RuntimeException("충전 후 보유 포인트는 최대 100만원을 초과할 수 없습니다.");
+            }
+
+            UserPoint chargedUserPoint = userPointTable.insertOrUpdate(id, originUserPoint.point() + amount);
+
+            pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+            return chargedUserPoint;
+        } finally {
+            lock.unlock();
         }
-        if(amount > MAX_AVAILABLE_POINT) {
-            throw new RuntimeException("충전 요청 금액은 최대 100만원을 초과할 수 없습니다.");
-        }
-
-        UserPoint originUserPoint = userPointTable.selectById(id);
-        if(originUserPoint.point() + amount > MAX_AVAILABLE_POINT) {
-            throw new RuntimeException("충전 후 보유 포인트는 최대 100만원을 초과할 수 없습니다.");
-        }
-
-        UserPoint chargedUserPoint = userPointTable.insertOrUpdate(id, originUserPoint.point() + amount);
-
-        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, updateMillis);
-
-        return chargedUserPoint;
     }
 
-    public UserPoint useUserPoint(long id, long amount, long updateMillis) {
-        if(amount <= 0) {
-            throw new RuntimeException("사용 요청 금액은 0원보다 커야 합니다.");
+    public UserPoint useUserPoint(long id, long amount) {
+        ReentrantLock lock = userLocks.computeIfAbsent(id, userId -> new ReentrantLock(true));
+
+        lock.lock();
+        try {
+            if(amount <= 0) {
+                throw new RuntimeException("사용 요청 금액은 0원보다 커야 합니다.");
+            }
+            if(amount > MAX_AVAILABLE_POINT) {
+                throw new RuntimeException("사용 요청 금액은 최대 100만원을 초과할 수 없습니다.");
+            }
+
+            UserPoint originUserPoint = userPointTable.selectById(id);
+            if(originUserPoint.point() - amount < 0) {
+                throw new RuntimeException("사용하려는 포인트는 보유한 포인트보다 클 수 없습니다.");
+            }
+
+            UserPoint usedUserPoint = userPointTable.insertOrUpdate(id, originUserPoint.point() - amount);
+
+            pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis());
+
+            return usedUserPoint;
+        } finally {
+            lock.unlock();
         }
-        if(amount > MAX_AVAILABLE_POINT) {
-            throw new RuntimeException("사용 요청 금액은 최대 100만원을 초과할 수 없습니다.");
-        }
-
-        UserPoint originUserPoint = userPointTable.selectById(id);
-        if(originUserPoint.point() - amount < 0) {
-            throw new RuntimeException("사용하려는 포인트는 보유한 포인트보다 클 수 없습니다.");
-        }
-
-        UserPoint usedUserPoint = userPointTable.insertOrUpdate(id, originUserPoint.point() - amount);
-
-        pointHistoryTable.insert(id, amount, TransactionType.USE, updateMillis);
-
-        return usedUserPoint;
     }
 }
